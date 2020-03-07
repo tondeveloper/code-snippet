d2b89bfec929d263d0352ba8b03bd3a77958c13c
JackTon <tondeveloper@gmail.com>
2020-03-07 02:10:10 -0600

    Create objectsetter.js

---
+++ src/objectsetter.js
@@ -0,0 +1,29 @@
+/*
+	Assign value to deep nested object using recursion and value reference
+*/
+
+const data = {
+  cat: {
+    dog: {
+      bird: 9
+    }
+  },
+  rabbit: [
+    {
+      dog: {
+        bird: 8
+      }
+    }
+  ]
+}
+
+const objSetter = (obj, value, path) => {
+  const [head, ...rest] = path.toString().split('.')
+  !rest.length
+    ? (obj[head] = value)
+    : objSetter(obj[head], value, rest.join('.'))
+}
+
+objSetter(data, 8888, 'rabbit.0.dog.bird')
+objSetter(data, 9999, 'cat.dog.bird')
+console.log(data)
